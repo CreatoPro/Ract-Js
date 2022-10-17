a3e64bff4f434728edc812d9294fd09e47f6d2fb
shreesat.sahu@allendigital.in+1-at-684168337650 <69542882+CreatoPro@users.noreply.github.com>
2022-10-17 18:26:00 +0530

    da

--- NETFLIX/api/index.js
+++ NETFLIX/api/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
+dotenv.config();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB is connected!"))
+    .catch((err) => console.log(err));
+
+  app.use(express.json());
+  app.use("/api/auth", authRoute);
+}
 app.listen(8000, () => {
   console.log("Backed server is running");
 });
